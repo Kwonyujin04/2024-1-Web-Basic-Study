1587ab90a6149a0fc8f01a9fdf9a686730b1c2d8
Kwonyujin04 <2310867@sookmyung.ac.kr>
2024-05-01 00:09:43 +0900

    Feat: 3주차 과제 및 실습 코드 추가 정보 작성
    	renamed:    "\352\266\214\354\234\240\354\247\204/3\354\243\274\354\260\250 \352\263\274\354\240\234/index_html.html" -> "\352\266\214\354\234\240\354\247\204/3\354\243\274\354\260\250 \352\263\274\354\240\234/index.html"
    	renamed:    "\352\266\214\354\234\240\354\247\204/3\354\243\274\354\260\250 \352\263\274\354\240\234/main_css.css" -> "\352\266\214\354\234\240\354\247\204/3\354\243\274\354\260\250 \352\263\274\354\240\234/main.css"
    	modified:   "\352\266\214\354\234\240\354\247\204/3\354\243\274\354\260\250 \352\263\274\354\240\234/pokemonCap.js"

--- 권유진/3주차 과제/pokemonCap.js
+++ 권유진/3주차 과제/pokemonCap.js
@@ -13,4 +13,22 @@ document.addEventListener("DOMContentLoaded", function() {
       alert("포켓몬" + pokemonOrder + "의 능력치는" + ability + "입니다. ");
     });
   });
+
+  function compareCap(){
+    const comparePokemon = {};
+    document.getElementById(".pokemon").onclick = 클릭한 값을 순서대로 comparePokemon에 객체에 넣어준다. ; //그리고 객체의 값이 2개 들어오면 입력을 마친다. 
+    if(첫번째 객체의 값 > 두번째 객체의 값){
+      console.log(첫번째 키값 + "이 이깁니다. ")
+    }
+    else if(첫번째 객체의 값 == 두번째 객체의 값){
+      console.log("둘의 능력치가 같습니다. ")
+    }
+    else(){
+      console.log(두번째 키값 + "이 이깁니다. ")
+    }
+  }
+
+  function slectPokemon(){
+
+  }
 });
