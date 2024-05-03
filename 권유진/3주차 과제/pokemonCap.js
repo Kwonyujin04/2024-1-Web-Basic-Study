e98a255047ded508c4b5e716815a4fd752daa6dc
Kwonyujin04 <2310867@sookmyung.ac.kr>
2024-05-03 12:52:26 +0900

    <Feat> 3주차 과제 및 실습 코드 수정 	modified:   "\352\266\214\354\234\240\354\247\204/3\354\243\274\354\260\250 \352\263\274\354\240\234/index.html"     modified:   "\352\266\214\354\234\240\354\247\204/3\354\243\274\354\260\250 \352\263\274\354\240\234/pokemonCap.js"

--- 권유진/3주차 과제/pokemonCap.js
+++ 권유진/3주차 과제/pokemonCap.js
@@ -1,34 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var myPokemons = document.querySelectorAll(".pokemon");
-  
+  const myPokemons = document.querySelectorAll(".container .pokemons .pokemon");	const pokemonlist = document.querySelectorAll(".container .pokemons .pokemon");
+  const pokemonCap = {};
+
   myPokemons.forEach(function(pokemon, index) {
-    pokemon.dataset.ability = Math.floor(Math.random() * 100) + 1;
-    pokemon.dataset.pokemonOrder = index + 1;
-  });
+    function randomCap(){
+      return Math.floor(Math.random() * 500) + 1;
+    }
+
+      const capacity = randomCap();
+      const pokemonNum = index + 1
+      pokemonCap[pokemonNum] = capacity;
   
-  myPokemons.forEach(function(pokemon) {
-    pokemon.addEventListener("click", function() {
-      var ability = this.dataset.ability;
-      var pokemonOrder = this.dataset.pokemonOrder;
-      alert("포켓몬" + pokemonOrder + "의 능력치는" + ability + "입니다. ");
-    });
+      pokemon.addEventListener("click", function() {
+        alert("포켓몬" + pokemonNum + "의 능력치는 " + pokemonCap[pokemonNum] + "입니다.");
+      });
   });
 
-  function compareCap(){
-    const comparePokemon = {};
-    document.getElementById(".pokemon").onclick = 클릭한 값을 순서대로 comparePokemon에 객체에 넣어준다. ; //그리고 객체의 값이 2개 들어오면 입력을 마친다. 
-    if(첫번째 객체의 값 > 두번째 객체의 값){
-      console.log(첫번째 키값 + "이 이깁니다. ")
-    }
-    else if(첫번째 객체의 값 == 두번째 객체의 값){
-      console.log("둘의 능력치가 같습니다. ")
-    }
-    else(){
-      console.log(두번째 키값 + "이 이깁니다. ")
-    }
-  }
+  const compareButton = document.getElementsByTagName("button");
 
-  function slectPokemon(){
+  compareButton.addEventListener("click", function(){
+    const pokemon1 = parseInt(prompt("Pokemon1을 입력해주세요 (숫자 입력)"));
+    const pokemon2 = parseInt(prompt("Pokemon2을 입력해주세요 (숫자 입력)"));
+    
+    if(isNaN(pokemon1) || isNaN(pokemon2) || pokemon1 < 1 || pokemon2 < 1 || pokemon1 > myPokemons.length || pokemon2 > myPokemons.length){
+      alert("올바른 값을 입력해주세요.");
+      return;
+    }
+    
+    const pokemonCap1 = parseInt(pokemonCap[pokemon1]);
+    const pokemonCap2 = parseInt(pokemonCap[pokemon2]);
 
-  }
-});
+    if(pokemonCap1>pokemonCap2){
+      alert("포켓몬 " + pokemon1 + "가 이깁니다.");
+    } else if(pokemonCap1 === pokemonCap2){
+      alert("무승부입니다.");
+    } else{
+      alert("포켓몬 " + pokemon2 + "가 이깁니다.");
+    }
+  })
+})
